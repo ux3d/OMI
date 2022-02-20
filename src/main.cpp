72fd0e539e62a80f77fb6cb833afaa0d4ea698f3
Norbert Nopper <nopper@ux3d.io>
2022-02-20 14:49:22 +0100

    Started with parsing code.
    Fixed tabs/spaces in the glTF.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,102 @@
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <vector>
+
 #include <AL/al.h>
 
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
+bool loadFile(std::string& output, const std::string& filename)
+{
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+	if (!file.is_open())
+	{
+		return false;
+	}
+
+	size_t fileSize = static_cast<size_t>(file.tellg());
+	file.seekg(0);
+
+	output.resize(fileSize);
+
+	file.read(output.data(), fileSize);
+	file.close();
+
+	return true;
+}
+
 int main(int argc, char *argv[])
 {
+	std::string parentPath = "./examples/";
+	std::string exampleFilename = "example01.gltf";
+
+	std::string loadFilename = parentPath + exampleFilename;
+
+	std::string gltfContent;
+	if (!loadFile(gltfContent, loadFilename))
+	{
+		printf("Error: Could not load glTF file '%s'\n", loadFilename.c_str());
+
+		return -1;
+	}
+
+	json glTF = json::parse(gltfContent);
+	if (!glTF.contains("extensions"))
+	{
+		printf("Error: glTF does not contain any extensions\n");
+
+		return -1;
+	}
+
+	json& extensions = glTF["extensions"];
+	if (!extensions.contains("OMI_audio_emitter"))
+	{
+		printf("Error: glTF does not contain the OMI_audio_emitter extension\n");
+
+		return -1;
+	}
+
+	// For now on, we expect the glTF does contain valid and the required data.
+
+	std::vector<std::string> audoSources;
+
+	json& OMI_audio_emitter = extensions["OMI_audio_emitter"];
+	for (auto& audioSource : OMI_audio_emitter["audioSources"])
+	{
+		if (!audioSource.contains("uri"))
+		{
+			printf("Error: Only supporting audioSource with uri\n");
+
+			return -1;
+		}
+
+		if (audioSource["uri"].get<std::string>().find("data:application/") == 0)
+		{
+			printf("Error: Only supporting audioSource with uri containing a filename\n");
+
+			return -1;
+		}
+
+		std::string uri = audioSource["uri"].get<std::string>();
+		loadFilename = parentPath + uri;
+
+		std::string mpeg;
+		if (!loadFile(mpeg, loadFilename))
+		{
+			printf("Error: Could not load mpeg file '%s'\n", loadFilename.c_str());
+
+			return -1;
+		}
+
+		printf("Info: Loaded audio source with uri '%s'\n", uri.c_str());
+	}
+
+	//
+
+	printf("Info: Done\n");
+
 	return 0;
 }
